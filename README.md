66b2283d8430eb15ec9ee46ca29a615296700e28
Dan Bosscher <daniel@289hq.com>
2019-01-31 14:58:24 +0000

    the first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+
+# Setup
+
+|Click link to run - use Edge |
+|---------|
+|<a href='http://boxstarter.org/package/url?https://raw.githubusercontent.com/danbosscher/windows-dev-box-setup-scripts/master/home.ps1'>Home machine</a>|
+|<a href='http://boxstarter.org/package/url?https://raw.githubusercontent.com/danbosscher/windows-dev-box-setup-scripts/master/work.ps1'>Work machine</a>|
+# Visitors
+
+This is the fork I use for my personal machine. YMMV. Please see the forked repo for instructions.
+
+# Legal
+
+Please read before using scripts.
+Our samples are provided AS-IS without any warranties of any kind.
+Chocolately has implemented security safeguards in their process to help protect the community from malicious or pirated software,but any use of our scripts is at your own risk.  Please read the Chocolately's legal terms of use and the Boxstarter project license as well as how the community repository for Chocolatey.org is maintained.
+Our project is subject to the MIT License and we make no warranties, express or implied of any kind.   In no event is Microsoft or contributing copyright holders be liable for any claim, damages or other liability arising from out of or in connection with the use of the project software or the use of other dealings in the project software.
---
+++ home.ps1
@@ -0,0 +1,76 @@
+# Description: Boxstarter Script
+# Author: Microsoft
+
+Disable-UAC
+$ConfirmPreference = "None" # Ensure installing powershell modules don't prompt on needed dependencies
+
+# Load external scripts
+# Get the base URI path from the ScriptToCall value
+$bstrappackage = "-bootstrapPackage"
+$helperUri = $Boxstarter['ScriptToCall']
+$strpos = $helperUri.IndexOf($bstrappackage)
+$helperUri = $helperUri.Substring($strpos + $bstrappackage.Length)
+$helperUri = $helperUri.TrimStart("'", " ")
+$helperUri = $helperUri.TrimEnd("'", " ")
+$helperUri = $helperUri.Substring(0, $helperUri.LastIndexOf("/"))
+$helperUri += "/scripts"
+write-host "helper script base URI is $helperUri"
+
+function executeScript {
+    Param ([string]$script)
+    write-host "executing $helperUri/$script ..."
+	iex ((new-object net.webclient).DownloadString("$helperUri/$script"))
+}
+
+#--- Running external scripts first ---
+executeScript "RemoveDefaultApps.ps1";
+executeScript "SystemConfiguration.ps1";
+
+#--- Setting up hardware ---
+choco install -y geforce-experience 
+
+#--- Setting up apps ---
+choco install -y firefox
+choco install -y ublockorigin-firefox
+choco install -y enpass.install
+choco install -y authy-desktop
+choco install -y telegram.install
+
+#--- Setting up work tools ---
+choco install -y vscode
+choco install -y greenshot
+choco install -y git --package-parameters="'/GitAndUnixToolsOnPath /WindowsTerminal'"
+choco install -y 7zip.install
+choco install -y powershell-core
+choco install -y cmdermini
+choco install -y curl
+choco install -y bitvise-ssh-client
+
+#--- Setting up Azure tools ---
+choco install -y azure-cli
+Install-PackageProvider -Name NuGet -Force
+Install-Module -Name Az -AllowClobber -Force
+choco install -y microsoftazurestorageexplorer
+
+#--- Setting up Games and media ---
+choco install -y vlc
+choco install -y steam
+choco install -y discord
+
+#--- Virtualisation et al ---
+# executeScript "HyperV.ps1";
+# RefreshEnv
+# executeScript "WSL.ps1";
+# RefreshEnv
+# executeScript "Docker.ps1";
+
+#--- Explorer settings ---
+Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowFileExtensions
+Set-TaskbarOptions -Size Small -Lock -Combine Always -AlwaysShowIconsOn
+
+#---Updates, UAC, Reboot.
+Disable-GameBarTips
+Disable-BingSearch
+Enable-MicrosoftUpdate
+Install-WindowsUpdate -acceptEula
+Enable-UAC
